d00225eef9503d2a77629038cf2f0c1b39941da7
sinfu <rsinfu@gmail.com>
2010-06-01 06:38:25 +0900

    Wrote a simple quine in D.

---
+++ quine.d
@@ -0,0 +1,5 @@
+// Written in the D programming language.
+import std.stdio; string s;
+void main() { writefln(s = q"<// Written in the D programming language.
+import std.stdio; string s;
+void main() { writefln(s = q"<%s>", s); }>", s); }
